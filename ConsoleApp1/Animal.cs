64af6ef3c17af9f1f0c15f6dbccebf3e4cd48cdc
kobisa16 <kobisa16@gmail.com>
2026-05-16 20:16:12 +0300

    hw4 trgil1

---
+++ ConsoleApp1/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Animal
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+       
+        public override string ToString()
+        {
+            return ($"Animal: {Name}, Age: {Age}");
+        }
+
+    }
+}
+
+
+
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Animal[] animals = new Animal[] { };
+
+
+
+            static void Print (Animal[] animals)
+            {
+                foreach (var item in animals)
+                {
+                    animals.ToString();
+
+                }
+
+                
+
+                
+
+
+            }
+        }
+    }
+}
